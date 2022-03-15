b8899da9de350e61d9f12905c1c80a7a6536961e
Sanidhya Singh <sanidhyas3s@gmail.com>
2022-03-15 14:44:31 +0530

    Builds the UI of the first 2 introduction pages

--- lib/screens/sign_in_screen.dart
+++ lib/screens/sign_in_screen.dart
@@ -1,87 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:provider/provider.dart';
-// import 'package:test/screens/home_page.dart';
-// import 'package:test/utils/routes.dart';
-// import 'package:test/utils/google_sign_in.dart';
-//
-// class Login_page extends StatefulWidget {
-//   @override
-//   State<Login_page> createState() => Login_pageState();
-// }
-//
-// class Login_pageState extends State<Login_page> {
-//   var name = "";
-//   @override
-//   Widget build(BuildContext context) {
-//     return Material(
-//       color: Colors.white,
-//       child: SingleChildScrollView(
-//         child: Column(
-//           children: [
-//             Image.asset(
-//               "lib/assets/Images/google_logo.png",
-//               fit: BoxFit.cover,
-//               height: 500,
-//             ),
-//             Text(
-//               "Welcome $name!",
-//               style: TextStyle(
-//                 fontSize: 23,
-//                 fontWeight: FontWeight.bold,
-//               ),
-//             ),
-//             Padding(
-//               padding: const EdgeInsets.all(16.0),
-//               child: Column(
-//                 children: [
-//                   TextFormField(
-//                     decoration: InputDecoration(
-//                       hintText: "Enter Your Username here",
-//                       labelText: "Username",
-//                     ),
-//                     onChanged: (value) {
-//                       name = value;
-//                       setState(() {});
-//                     },
-//                   ),
-//                   TextFormField(
-//                     obscureText: true,
-//                     decoration: InputDecoration(
-//                       hintText: "Enter Your Password here",
-//                       labelText: "Password",
-//                     ),
-//                   ),
-//                   ElevatedButton(
-//                     onPressed: () {
-//                       final provider =Provider.of<GoogSignIn>(context, listen: false);
-//                       provider.googleLogin();
-//                       // Navigator.pushNamed(context, MyRoutes.home_route);
-//                     },
-//                     child: Text("Sign up with Google"),
-//                     style: ElevatedButton.styleFrom(
-//                         onPrimary: Colors.orange  ,
-//                         primary: Colors.white,
-//                         minimumSize: Size(150, 40)),
-//                   ),
-//                 ],
-//               ),
-//             ),
-//             Image.asset(
-//               "lib/assets/Images/google_logo.png",
-//               fit: BoxFit.cover,
-//               height: 500,
-//             ),
-//             Image.asset(
-//               "lib/assets/Images/google_logo.png",
-//               fit: BoxFit.cover,
-//               height: 500,
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import '/screens/sign_in_screen.dart';
@@ -121,14 +37,9 @@ class _SignInScreenState extends State<SignInScreen> {
                   mainAxisSize: MainAxisSize.min,
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    //   Image.asset(
-                    //     'assets/google_logo.png',
-                    //     height: 4,
-                    //     width: 4,
-                    // ),
                     SizedBox(height: 4),
                     Text(
-                      'Auito',
+                      'auito',
                       style: TextStyle(
                         color: Colors.amber,
                         fontSize: 40,
